7d886104e0374098ad8599bf8101434ce1e9c451
Vadim <fates@duck.com>
2026-05-29 20:29:42 +0300

    Fixed a bug when long test names in rerun plugin could cause invalid reruns (#2)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ All notable changes to this project are documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## Unreleased
+
+### Fixed
+
+- Fixed a bug when long test names in rerun plugin could cause invalid reruns.
+
 ## [1.1.0] - 2026-05-24
 
 ### Added
--- rerun/plugin.go
+++ rerun/plugin.go
@@ -3,6 +3,7 @@ package rerun
 
 import (
 	"flag"
+	"path"
 	"sync"
 
 	"github.com/ozontech/testo"
@@ -56,7 +57,7 @@ func (pr *PluginRerun) beforeAll() testoplugin.Hook {
 					r := testo.Reflect(pr)
 
 					s := suite{
-						Name:   r.Suite.Caller + "/" + r.Suite.Name,
+						Name:   r.Suite.Caller + keySep + r.Suite.Name,
 						Failed: pr.Failed(),
 					}
 
@@ -83,11 +84,19 @@ func (pr *PluginRerun) beforeAll() testoplugin.Hook {
 
 			suite := testo.Reflect(pr).Suite
 
-			if !c.Suites[suite.Caller+"/"+suite.Name].Failed {
-				pr.Skipf(
-					"rerun: there are no known test failures for suite %q, skipping",
-					suite.Name,
-				)
+			if !c.Suites[suite.Caller+keySep+suite.Name].Failed {
+				// inside a suiteless test
+				if suite.Name == "" {
+					pr.Skipf(
+						"rerun: there is no known test failure for test %q, skipping",
+						path.Base(suite.Caller),
+					)
+				} else {
+					pr.Skipf(
+						"rerun: there are no known test failures for suite %q, skipping",
+						suite.Name,
+					)
+				}
 			}
 		},
 	}
