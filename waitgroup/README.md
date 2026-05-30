2cc04a32ad0cead3e19b083ad3c640a2c9cbf2ab
metafates <vvz.dev@gmail.com>
2026-05-30 16:11:36 +0300

    improve doc

---
+++ waitgroup/README.md
@@ -0,0 +1,9 @@
+# 🕰️ Waitgroup
+
+Provides a test‑aware `sync.WaitGroup`.
+
+## Quick Start
+
+```bash
+go get github.com/ozontech/testo-toppings
+```
---
+++ waitgroup/plugin.go
@@ -0,0 +1,111 @@
+// Package waitgroup provides a test‑aware [sync.WaitGroup].
+package waitgroup
+
+import (
+	"sync"
+
+	"github.com/ozontech/testo"
+	"github.com/ozontech/testo/testoplugin"
+)
+
+var (
+	_ CommonT            = (*PluginWaitGroup)(nil)
+	_ testoplugin.Plugin = (*PluginWaitGroup)(nil)
+)
+
+// CommonT is an interface common for all Ts with [PluginWaitGroup] installed.
+type CommonT interface {
+	testo.CommonT
+
+	Go(f func())
+	Wait()
+
+	unwrapWaitGroup() *PluginWaitGroup
+}
+
+// PluginWaitGroup simplifies goroutine spawning in tests.
+type PluginWaitGroup struct {
+	*testo.T
+
+	wg sync.WaitGroup
+}
+
+// Plugin implements [testoplugin.Plugin].
+func (wg *PluginWaitGroup) Plugin(testoplugin.Plugin, ...testoplugin.Option) testoplugin.Spec {
+	return testoplugin.Spec{
+		Hooks: testoplugin.Hooks{
+			AfterAll:     wg.after(),
+			AfterEach:    wg.after(),
+			AfterEachSub: wg.after(),
+		},
+	}
+}
+
+func (wg *PluginWaitGroup) after() testoplugin.Hook {
+	return testoplugin.Hook{
+		Priority: testoplugin.TryFirst,
+		Func:     wg.Wait,
+	}
+}
+
+// Wait blocks until all functions started from [PluginWaitGroup]
+// are finished.
+//
+// Note, that calling this function is optional, as it will be called
+// by the plugin after the current test or sub-test ends.
+func (wg *PluginWaitGroup) Wait() {
+	wg.wg.Wait()
+}
+
+// Go calls f in a new goroutine and adds that task to the [sync.WaitGroup].
+// When f returns, the task is removed from the WaitGroup.
+//
+// All tasks are awaited before test completion with [sync.WaitGroup.Wait].
+// Use [PluginWaitGroup.Wait] to manually await all running goroutines.
+//
+// The function f must not panic.
+func (wg *PluginWaitGroup) Go(f func()) {
+	// TODO(metafates): use [sync.WaitGroup.Go] once available.
+
+	wg.wg.Add(1)
+
+	go func() {
+		wg.Helper()
+
+		defer func() {
+			if x := recover(); x != nil {
+				// f panicked, which will be fatal because
+				// this is a new goroutine.
+				//
+				// Calling Done will unblock Wait in the main goroutine,
+				// allowing it to race with the fatal panic and
+				// possibly even exit the process (os.Exit(0))
+				// before the panic completes.
+				//
+				// This is almost certainly undesirable,
+				// so instead avoid calling Done and simply panic.
+				panic(x)
+			}
+
+			wg.wg.Done()
+		}()
+
+		f()
+	}()
+}
+
+func (wg *PluginWaitGroup) unwrapWaitGroup() *PluginWaitGroup {
+	return wg
+}
+
+// Run calls [testo.Run] inside [PluginWaitGroup.Go] and returns immediately.
+//
+// All tasks are awaited before test completion with [sync.WaitGroup.Wait].
+// Use [PluginWaitGroup.Wait] to manually await all running goroutines.
+func Run[T CommonT](t T, name string, f func(t T), options ...testoplugin.Option) {
+	t.unwrapWaitGroup().Go(func() {
+		t.Helper()
+
+		testo.Run(t, name, f, options...)
+	})
+}
